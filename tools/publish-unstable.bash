68c7ed26a1f654007fd5878b72a7e88946cd0ced
Stephen Niedzielski <stephen@niedzielski.com>
2023-09-14 21:02:26 -0600

    [DX-4021] Drop time from version
    Drop time as base number seems unremovable.
    
    https://docs.npmjs.com/cli/v9/commands/npm-version
    https://github.com/npm/node-semver#functions

--- tools/publish-unstable.bash
+++ tools/publish-unstable.bash
@@ -2,20 +2,13 @@
 # Publish a development release (@next). See docs/publishing.md.
 #
 # Environment variables:
-# - VERSION_PRE_ID: Prerelease package version identifier for reproduction.
-#                   Defaults to current timestamp.
 # - V: Verbose mode. Defaults to off.
 #
-# Example (if main is v0.0.0, publish v0.0.1-dev+2000-01-02T0304):
+# Example (if main is v0.0.0, publish v0.0.1-dev+2000-01-02.3):
 #
 #   tools/publish-unstable
 
 set -${V:+x}
-
-# A timestamp is used to avoid conflicts as only one tag or version can ever
-# exist. Additionally, the special NPM tags, `@latest` and `@next`, cannot point
-# to the same version.
-VERSION_PRE_ID="${VERSION_PRE_ID:-dev+$(TZ=utc date +%FT%H%M)}"
 
 # Development tags are not wanted on the main branch. Checkout `HEAD` detached.
 git fetch origin main
@@ -25,7 +18,7 @@ git checkout origin/main
 npm ci
 
 # This will create a new version commit on the detached `HEAD` like
-# `v1.2.4-dev+2000-01-02T0304`.
-npm version prerelease --preid="$VERSION_PRE_ID"
+# `v1.2.4-dev+2000-01-02.3`.
+npm version prerelease --preid="-dev+$(TZ=utc date +%F)"
 
 npm publish --tag next
