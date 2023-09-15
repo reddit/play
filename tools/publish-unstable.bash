8288e0450307a879b941b11167233efa0f0b9241
Stephen Niedzielski <stephen@niedzielski.com>
2023-09-14 21:56:38 -0600

    [DX-4021] Fix up unstable publishing comments

--- tools/publish-unstable.bash
+++ tools/publish-unstable.bash
@@ -4,7 +4,7 @@
 # Environment variables:
 # - V: Verbose mode. Defaults to off.
 #
-# Example (if main is v0.0.0, publish v0.0.1-dev+2000-01-02T0304.0):
+# Example (if main is v0.0.0, publish v0.0.1-dev.2000-01-02T0304.0):
 #
 #   tools/publish-unstable
 
@@ -21,8 +21,8 @@ npm run clean
 npm ci
 
 # This will create a new version commit on the detached `HEAD` like
-# `v1.2.4-dev+2000-01-02T0304.0`. Time is used to prevent collision, The base
-# number (.0) seems fixed.
+# `v1.2.4-dev.2000-01-02T0304.0`. Time is used to prevent collision by passing
+# as a prerelease version instead of metadata. The base number (.0) seems fixed.
 npm version prerelease --preid="dev.$(TZ=utc date +%FT%H%M)"
 
 npm publish --tag next
