7a1a9d43052461e7b2dcad3eab914793aa7a603c
Stephen Niedzielski <stephen@niedzielski.com>
2023-09-20 22:35:28 -0600

    Move some pen styles in

--- src/ui/components/play-pen.ts
+++ src/ui/components/play-pen.ts
@@ -1,7 +1,7 @@
 import {LitElement, css, html} from 'lit'
 import {customElement, property, query} from 'lit/decorators.js'
-import type {PlayPenContextProvider} from './play-pen-context-provider.js'
 import type {PlayEditor} from './play-editor.js'
+import type {PlayPenContextProvider} from './play-pen-context-provider.js'
 
 import './play-editor.js'
 import './play-pen-context-provider.js'
@@ -22,13 +22,51 @@ declare global {
 @customElement('play-pen')
 export class PlayPen extends LitElement {
   static override styles = css`
+    :host {
+      /* to-do: Support light and dark mode. */
+      /* color-scheme: light dark; */
+
+      /* Global color definitions. */
+      --rpl-global-orangered: #ff4500;
+      --rpl-orangered-100: #ffede5;
+
+      /* #theme# Light mode color definitions. */
+      --rpl-neutral-content-weak: #576f76;
+      --rpl-neutral-content-strong: #0f1a1c;
+      --rpl-neutral-background-weak: #f9fafa;
+      --rpl-neutral-background-weak-hovered: #f2f4f5;
+      --rpl-neutral-background: #ffffff;
+      --rpl-neutral-background-hovered: #f9fafa;
+      --rpl-neutral-border: rgba(0, 0, 0, 0.2);
+      --rpl-brand-background: #d93a00;
+      --rpl-brand-background-hovered: #962900;
+      --rpl-brand-background-pressed: #7e2200;
+      --rpl-brand-onBackground: #ffffff;
+      --rpl-secondary-plain: #0f1a1c;
+      --rpl-secondary-background: #eaedef;
+      --rpl-secondary-background-hovered: #e2e7e9;
+      --rpl-secondary-background-pressed: #bec2c4;
+      --rpl-secondary-onBackground: #000000;
+
+      /* #theme# Light mode. */
+      color: #213547;
+      background-color: var(--rpl-neutral-background);
+    }
+
+    @media (prefers-color-scheme: dark) {
+      :host {
+        /* #theme# Dark mode. */
+        /* color: rgba(255, 255, 255, 0.87);
+        background-color: #242424; */
+      }
+    }
+
     play-pen-context-provider {
       width: 100%;
       height: 100%;
       display: flex;
       flex-direction: column;
       box-shadow: 0 0 0 24px var(--rpl-brand-background);
-      background-color: var(--rpl-neutral-background);
     }
 
     play-editor {
@@ -49,7 +87,6 @@ export class PlayPen extends LitElement {
       padding-bottom: 24px;
       padding-left: 24px;
       height: 100%;
-      background-color: var(--rpl-neutral-background);
     }
   `
 
