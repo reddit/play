186885007f1d887723e1575d4499acaaa336e328
Stephen Niedzielski <stephen@niedzielski.com>
2023-09-22 17:30:46 -0600

    Trim whitespace

--- src/ui/components/play-app.ts
+++ src/ui/components/play-app.ts
@@ -31,41 +31,39 @@ export class PlayApp extends LitElement {
   }
 
   protected override render() {
-    return html`
-      <play-pen allow-storage allow-url>
-        <script lang="tsx" type="application/devvit">
-          import {Devvit} from '@devvit/public-api'
+    return html`<play-pen allow-storage allow-url>
+      <script lang="tsx" type="application/devvit">
+        import {Devvit} from '@devvit/public-api'
 
-          Devvit.addCustomPostType({
-            name: 'Say Hello',
-            render: ctx => {
-              const [counter, setCounter] = ctx.useState(0)
-              return (
-                <vstack alignment='center middle' height={100}>
-                  <hstack alignment='center middle' gap='medium' padding='medium'>
-                    <text size='xxlarge' style='heading'>
-                      Hello! 👋
-                    </text>
-                    <button
-                      appearance='primary'
-                      onPress={() => setCounter(counter => counter + 1)}
-                    >
-                      Click me!
-                    </button>
-                  </hstack>
-                  {counter ? (
-                    <text>{\`You clicked \${counter} time(s)!\`}</text>
-                  ) : (
-                    <text>&nbsp;</text>
-                  )}
-                </vstack>
-              )
-            }
-          })
+        Devvit.addCustomPostType({
+          name: 'Say Hello',
+          render: ctx => {
+            const [counter, setCounter] = ctx.useState(0)
+            return (
+              <vstack alignment='center middle' height={100}>
+                <hstack alignment='center middle' gap='medium' padding='medium'>
+                  <text size='xxlarge' style='heading'>
+                    Hello! 👋
+                  </text>
+                  <button
+                    appearance='primary'
+                    onPress={() => setCounter(counter => counter + 1)}
+                  >
+                    Click me!
+                  </button>
+                </hstack>
+                {counter ? (
+                  <text>{\`You clicked \${counter} time(s)!\`}</text>
+                ) : (
+                  <text>&nbsp;</text>
+                )}
+              </vstack>
+            )
+          }
+        })
 
-          export default Devvit
-        </script>
-      </play-pen>
-    `
+        export default Devvit
+      </script>
+    </play-pen>`
   }
 }
