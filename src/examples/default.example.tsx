39b4cb6c404ca11ab3715454eb1a180f8e38ebd8
Stephen Niedzielski <stephen@oidoid.com>
2023-10-05 13:56:23 -0600

    Tweak examples

--- src/examples/default.example.tsx
+++ src/examples/default.example.tsx
@@ -3,7 +3,10 @@ import {Devvit} from '@devvit/public-api'
 Devvit.addCustomPostType({
   name: 'Name',
   render: _context => {
-    return <blocks></blocks>
+    return (
+      <blocks>
+      </blocks>
+    )
   }
 })
 
--- src/examples/hello-blocks.example.tsx
+++ src/examples/hello-blocks.example.tsx
@@ -5,24 +5,22 @@ Devvit.addCustomPostType({
   render: context => {
     const [counter, setCounter] = context.useState(0)
     return (
-      <blocks>
-        <vstack alignment='center middle' height='100%' gap='large'>
-          <text size='xxlarge' weight='bold'>
-            Hello! 👋
-          </text>
-          <button
-            appearance='primary'
-            onPress={() => setCounter(counter => counter + 1)}
-          >
-            Click me!
-          </button>
-          {counter ? (
-            <text>{`You clicked ${counter} time(s)!`}</text>
-          ) : (
-            <text>&nbsp;</text>
-          )}
-        </vstack>
-      </blocks>
+      <vstack alignment='center middle' height='100%' gap='large'>
+        <text size='xxlarge' weight='bold'>
+          Hello! 👋
+        </text>
+        <button
+          appearance='primary'
+          onPress={() => setCounter(counter => counter + 1)}
+        >
+          Click me!
+        </button>
+        {counter ? (
+          <text>{`You clicked ${counter} time(s)!`}</text>
+        ) : (
+          <text>&nbsp;</text>
+        )}
+      </vstack>
     )
   }
 })
