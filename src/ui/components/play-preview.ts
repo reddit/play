109cf59a5b09efb3f1c962e4e969ad798086f387
Perron Jones <perron.jones@reddit.com>
2023-12-19 09:58:08 -0500

    connect to web socket to forward updated bundles

--- src/ui/components/play-preview.ts
+++ src/ui/components/play-preview.ts
@@ -1,6 +1,6 @@
 import penWorker from '@devvit/previews/dist/pen.worker.min.js'
-import type {LinkedBundle, Metadata} from '@devvit/protos'
-import {BrowserLiteClient} from '@devvit/runtime-lite/BrowserLiteClient.js'
+import type { LinkedBundle, Metadata } from '@devvit/protos'
+import { BrowserLiteClient } from '@devvit/runtime-lite/BrowserLiteClient.js'
 import {
   LitElement,
   css,
@@ -9,12 +9,12 @@ import {
   type PropertyValues,
   type TemplateResult
 } from 'lit'
-import {customElement, property, state} from 'lit/decorators.js'
-import type {ColorScheme} from '../../types/color-scheme.js'
-import {Bubble} from '../bubble.js'
+import { customElement, property, state } from 'lit/decorators.js'
+import type { ColorScheme } from '../../types/color-scheme.js'
+import { Bubble } from '../bubble.js'
 
 import '@devvit/previews/dist/devvit-preview.js'
-import type {PreviewError} from '../../types/preview-error.js'
+import type { PreviewError } from '../../types/preview-error.js'
 
 declare global {
   interface HTMLElementEventMap {
@@ -24,6 +24,61 @@ declare global {
   }
   interface HTMLElementTagNameMap {
     'play-preview': PlayPreview
+  }
+}
+
+function debounce(fn: Function, ms = 1000) {
+  let timeoutId: NodeJS.Timeout;
+  return function (this: any, ...args:any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
+
+let socket: WebSocket;
+let sendCount = 0;
+function initSocket(): void {
+  console.log('Initializing Socket for client updates');
+  if (socket) return;
+  sendCount = 0
+
+  // Can't use server events due to CORS
+  try {
+    socket = new WebSocket('ws://localhost:5678');
+  } catch (err) {
+    console.log('playtest unable to connect.' );
+    return;
+  }
+
+  socket.onopen = () => {
+    console.log('playtest connected.');
+  };
+
+  socket.onclose = () => {
+    console.log( 'playtest disconnected.');
+  };
+
+  socket.onmessage = ev => {
+    const msg = JSON.parse(ev.data);
+    console.log(msg);
+    // to-do: filter on install ID and pre-release versions so updates only
+    // trigger where relevant.
+
+  };
+}
+
+const updateBundle = debounce((bundle: Readonly<LinkedBundle> | undefined) => sendNewBundle(bundle))
+
+function sendNewBundle(bundle: Readonly<LinkedBundle> | undefined) {
+  sendCount++;
+  console.log('send count: ', sendCount);
+  if (socket && socket.readyState == socket.OPEN) {
+    try {
+      console.log('sending new bundle');
+      socket.send(JSON.stringify(bundle));
+    } catch (err) {
+      console.error('Failed to send new bundle');
+    }
   }
 }
 
@@ -75,18 +130,23 @@ export class PlayPreview extends LitElement {
     }
   `
 
-  @property({attribute: false}) bundle: Readonly<LinkedBundle> | undefined
-  @property({type: Number}) previewWidth?: number
+  @property({ attribute: false }) bundle: Readonly<LinkedBundle> | undefined
+  @property({ type: Number }) previewWidth?: number
   @property() scheme?: ColorScheme
 
   @state() private readonly _client: BrowserLiteClient = new BrowserLiteClient(
-    new Blob([penWorker], {type: 'text/javascript'}),
-    (type, err) => this.dispatchEvent(Bubble('error', {type, err}))
+    new Blob([penWorker], { type: 'text/javascript' }),
+    (type, err) => this.dispatchEvent(Bubble('error', { type, err }))
   )
   #meta: Metadata = {
-    'devvit-app-user': {values: ['t2_appuser']},
-    'devvit-subreddit': {values: ['t5_devvit']},
-    'devvit-user': {values: ['t2_user']}
+    'devvit-app-user': { values: ['t2_appuser'] },
+    'devvit-subreddit': { values: ['t5_devvit'] },
+    'devvit-user': { values: ['t2_user'] }
+  }
+
+  constructor() {
+    super()
+    initSocket()
   }
 
   async reset(): Promise<void> {
@@ -96,10 +156,10 @@ export class PlayPreview extends LitElement {
     try {
       await this._client.loadBundle(this.bundle)
     } catch (err) {
-      this.dispatchEvent(Bubble('error', {type: 'Error', err}))
+      this.dispatchEvent(Bubble('error', { type: 'Error', err }))
     }
     // Re-render the preview.
-    this.#meta = {...this.#meta}
+    this.#meta = { ...this.#meta }
     this.requestUpdate()
   }
 
@@ -109,13 +169,15 @@ export class PlayPreview extends LitElement {
   }
 
   protected override render(): TemplateResult {
+    console.log("Updating render")
+    updateBundle(this.bundle)
     // to-do: don't override toaster's --rem16 to offset the toast. Upstream a
     // variable.
     return html`<div class="preview">
       ${this.bundle &&
       html`<devvit-preview
         @devvit-ui-error=${(ev: CustomEvent<unknown>) =>
-          this.dispatchEvent(Bubble('error', {type: 'Error', err: ev.detail}))}
+          this.dispatchEvent(Bubble('error', { type: 'Error', err: ev.detail }))}
         .meta="${this.#meta}"
         .client=${this._client}
         .scheme=${this.scheme}
