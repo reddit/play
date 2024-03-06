55178346fc9eb187909f8dd7f13c183861526bae
Stephen Niedzielski <stephen@oidoid.com>
2024-03-06 14:56:12 -0700

    Update CustomPost and UIEventHandler SerializableServiceDefinitions

--- src/bundler/linker.ts
+++ src/bundler/linker.ts
@@ -15,9 +15,27 @@ function provides(): SerializableServiceDefinition[] {
           fullName: '/devvit.reddit.custom_post.v1alpha.CustomPost/RenderPost',
           name: 'RenderPost',
           requestStream: false,
-          requestType: 'devvit.reddit.custom_post.v1alpha.RenderPostRequest',
           responseStream: false,
+          requestType: 'devvit.reddit.custom_post.v1alpha.RenderPostRequest',
           responseType: 'devvit.reddit.custom_post.v1alpha.RenderPostResponse'
+        },
+        {
+          fullName:
+            '/devvit.reddit.custom_post.v1alpha.CustomPost/RenderPostContent',
+          name: 'RenderPostContent',
+          requestStream: false,
+          responseStream: false,
+          requestType: 'devvit.ui.block_kit.v1beta.UIRequest',
+          responseType: 'devvit.ui.block_kit.v1beta.UIResponse'
+        },
+        {
+          fullName:
+            '/devvit.reddit.custom_post.v1alpha.CustomPost/RenderPostComposer',
+          name: 'RenderPostComposer',
+          requestStream: false,
+          responseStream: false,
+          requestType: 'devvit.ui.block_kit.v1beta.UIRequest',
+          responseType: 'devvit.ui.block_kit.v1beta.UIResponse'
         }
       ],
       name: 'CustomPost',
@@ -30,8 +48,8 @@ function provides(): SerializableServiceDefinition[] {
           fullName: '/devvit.ui.events.v1alpha.UIEventHandler/HandleUIEvent',
           name: 'HandleUIEvent',
           requestStream: false,
-          requestType: 'devvit.ui.events.v1alpha.HandleUIEventRequest',
           responseStream: false,
+          requestType: 'devvit.ui.events.v1alpha.HandleUIEventRequest',
           responseType: 'devvit.ui.events.v1alpha.HandleUIEventResponse'
         }
       ],
